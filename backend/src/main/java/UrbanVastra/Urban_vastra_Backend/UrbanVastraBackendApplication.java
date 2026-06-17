a1c552ccd70e73e17c6928a34dd0c2799e52c188
Kushagra Goel <kushagragoelv.2@gmail.com>
2026-06-17 15:49:42 +0530

    Restructure project into frontend and backend modules

---
+++ backend/src/main/java/UrbanVastra/Urban_vastra_Backend/UrbanVastraBackendApplication.java
@@ -0,0 +1,13 @@
+package UrbanVastra.Urban_vastra_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanVastraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanVastraBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/UrbanVastra/Urban_vastra_Backend/UrbanVastraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package UrbanVastra.Urban_vastra_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanVastraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
